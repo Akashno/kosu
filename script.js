7f0fa86a8ffbe8bd05864aafd4ba88c4cbbf51fc
Akash N O <akash@arminotech.in>
2023-01-15 20:03:57 +0530

    kosu and thundachi treat

---
+++ script.js
@@ -0,0 +1,29 @@
+var fallingTextArr = [];
+
+function fall(text) {
+  var top = 0;
+  var intervalId = setInterval(frame, 10);
+  function frame() {
+    if (top >= window.innerHeight) {
+      clearInterval(intervalId);
+      document.body.removeChild(text);
+      fallingTextArr.splice(fallingTextArr.indexOf(text), 1);
+    } else {
+      top++;
+      text.style.top = top + "px";
+    }
+  }
+}
+
+function addFallingText() {
+  var text = document.createElement("div");
+  text.innerHTML = "🖕";
+  text.classList.add("falling-text");
+  text.style.fontSize ="20px" 
+  text.style.left = Math.random() * (window.innerWidth - text.clientWidth) + "px";
+  document.body.appendChild(text);
+  fallingTextArr.push(text);
+  fall(text);
+}
+
+setInterval(addFallingText, 1000);
